0016bd383cb4c3ed0555967ffbcdeddd14794243
Pavan Parvatam <pavankumar41299@gmail.com>
2025-09-26 01:10:34 +0530

    Location contact

--- src/components/Venue/VenueContent.tsx
+++ src/components/Venue/VenueContent.tsx
@@ -26,19 +26,21 @@ function AttendContent() {
         </div>
 
         {/* Heading */}
-        <section className="w-full py-8 md:py-14 px-4">
+        <section className="w-full py-2 md:py-6 px-4">
           <h2 className="font-inter font-semibold text-[24px] sm:text-[28px] md:text-[32px] lg:text-[40px] leading-[36px] sm:leading-[42px] md:leading-[48px] lg:leading-[60px] text-center text-[#101828] mb-6 md:mb-10">
             Pride Plaza Hotel, Aerocity, New Delhi
           </h2>
 
           {/* Venue Description */}
-          <p className="font-inter text-[16px] md:text-[18px] leading-[28px] text-[#475467] text-justify max-w-4xl mx-auto">
-          Asset 5-A, Hospitality District, Indira Gandhi International Airport, Aerocity, New Delhi, Delhi 110037.
+          <p className="font-inter text-center text-[16px] md:text-[18px] leading-[28px] text-[#475467] max-w-4xl mx-auto">
+            Asset 5-A, Hospitality District, Indira Gandhi International Airport, Aerocity, New Delhi, Delhi 110037.
+            <br />
+            Contact : 1800 209 1400
           </p>
+
         </section>
       
-        <div className="flex items-start gap-4 mb-6 ">
-        
+        <div className="flex items-start gap-4 mb-2 ">        
         <a
           href="https://maps.app.goo.gl/McaH173NtMpq6kPeA"
           target="_blank"
