4a7d2b4329341dc9ae726b2776bb1ca5790b5a8c
Pavan Parvatam <pavankumar41299@gmail.com>
2025-11-17 14:08:47 +0530

    Updated Journal Track

--- src/components/JournalTrack/AboutJournalTrack.tsx
+++ src/components/JournalTrack/AboutJournalTrack.tsx
@@ -34,6 +34,7 @@ function AboutJournalTrack() {
           </div>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-4">
           We invite submissions to the journal track of the 2026 IEEE International Conference on Data Science and Advanced Analytics (DSAA&apos;2026). This journal track is implemented in partnership with two Springer&apos;s journals, namely: Machine Learning Journal and International Journal of Data Science and Analytics (JDSA). A paper can only be submitted to either, but not both, journals. Accepted papers will be published in the corresponding journal, each with an extended abstract included in the DSAA&apos;2026 proceedings.
+
           </div>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-4">
           <ul className="list-disc list-inside font-inter text-[15px] md:text-[16px] text-[#232B3A] space-y-2">            
@@ -41,6 +42,13 @@ function AboutJournalTrack() {
           </ul>
           <span className="font-semibold"></span>
           The DSAA&apos;2026 Journal Track with the Machine Learning Journal will consolidate original submissions to the Special Issue on Large Language Modelling for Data Science. The Special Issue explores the intersection between LLMs and data science, addressing foundational advances and real-world applications. We invite high-quality contributions that extend the methodological, practical, and theoretical boundaries of how LLMs can accelerate discovery, enhance interpretability, and enable novel paradigms in data-driven science and engineering.
+          For the full call for papers and submissions, please refer 
+          <a
+          href="https://link.springer.com/collections/jgifhigcbd"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#1D4ED8] hover:underline"
+          > Guidelines Here.</a>
           </div>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-4">
           <ul className="list-disc list-inside font-inter text-[15px] md:text-[16px] text-[#232B3A] space-y-2">            
@@ -48,6 +56,14 @@ function AboutJournalTrack() {
           </ul>
           <span className="font-semibold"></span>
           A priority consideration is given to Data Science and Generative AI. This theme explores the bidirectional relationship between data science and generative AI. We invite contributions that advance the foundations of data science to support the development, training, evaluation, and responsible deployment of generative AI models, including work on data representation, curation, quality, interpretability, fairness, and governance. At the same time, we welcome research that leverages generative AI to transform data science practices, such as data preparation, augmentation, simulation, automated analysis, hypothesis generation, and scientific discovery. Submissions may cover theoretical advances, methodological innovations, system development, or novel applications that highlight how data science and generative AI mutually reinforce each other in both research and practice.
+          For the full call for papers and submissions, please refer   
+          <a
+          href="https://link.springer.com/journal/41060/updates/27811948"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#1D4ED8] hover:underline"
+          > Guidelines Here.</a>
+          
           </div>
           
 {/*           
@@ -59,7 +75,7 @@ function AboutJournalTrack() {
         </div>
         
         {/* Paper Submissions */}
-        <div className="border border-[#D1D5DB] rounded-xl p-6 md:p-8 mb-6 max-w-5xl mx-auto">
+        {/* <div className="border border-[#D1D5DB] rounded-xl p-6 md:p-8 mb-6 max-w-5xl mx-auto">
   <h3 className="font-inter font-semibold text-[22px] md:text-[26px] text-[#232B3A] mb-4">
     Paper Submissions
   </h3>
@@ -107,7 +123,7 @@ function AboutJournalTrack() {
     to arXiv papers that were published in arXiv at least one month prior to the DSAA&apos;2026 
     submission deadline, provided that the submitted paper&apos;s title and abstract differ from the arXiv version.
   </p>
-</div>
+</div> */}
 
         {/* <div className="border border-[#D1D5DB] rounded-xl p-6 md:p-8 mb-6 max-w-5xl mx-auto">
           <h3 className="font-inter font-semibold text-[22px] md:text-[26px] text-[#232B3A] mb-3">
@@ -136,60 +152,9 @@ function AboutJournalTrack() {
         </div> */}
 
         {/* Proceedings and Indexing */}
-        <div className="border border-[#D1D5DB] rounded-xl p-6 md:p-8 mb-6 max-w-5xl mx-auto">
-          <h3 className="font-inter font-semibold text-[22px] md:text-[26px] text-[#232B3A] mb-3">
-            Proceedings and Indexing
-          </h3>
-            <p className="font-inter text-[15px] md:text-[16px] text-[#232B3A] leading-relaxed">
-              All accepted full-length papers will be published by IEEE and will be submitted 
-              for inclusion in the IEEE Xplore Digital Library. The conference proceedings will also be 
-              submitted for EI indexing through INSPEC by IEEE.
-            </p>
-          {/* <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A]">
-            All accepted full-length papers and short papers will be published by <span className="font-semibold">IEEE</span>, submitted for inclusion in the <span className="font-semibold">IEEE Xplore Digital Library</span>, and for EI indexing. through.<span className="font-semibold">INSPEC</span>.
-          </div> */}
-        </div>
-
+       
         {/* Important Policies */}
-        <div className="border border-[#D1D5DB] rounded-xl p-6 md:p-8 mb-6 max-w-5xl mx-auto">
-          <h3 className="font-inter font-semibold text-[22px] md:text-[26px] text-[#232B3A] mb-3">
-            Important Policies
-          </h3>
-          <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-3">
-            All papers should be submitted electronically.
-          </div>
-          <ol className="font-inter text-[15px] md:text-[16px] text-[#232B3A] list-decimal pl-5 mb-3 space-y-1">
-            <li>
-              <span className="font-semibold">Reproducibility & Supplementary</span>  The advancement of data science and analytics depends heavily on reproducibility. We strongly recommend that the authors release their code and data to the public. Authors can provide an optional two (2) page supplement at the end of their submitted paper (it needs to be in the same PDF file after the paper&apos;s references). This supplement can only be used to include (i) information necessary for reproducing the experimental results reported in the paper (e.g., various algorithmic and model parameters and configurations, hyper-parameter search spaces, details related to dataset filtering and train/test splits, software versions, detailed hardware configuration, etc.), and (ii) any data, pseudo-code and proofs that due to space limitations, could not be included in the main manuscript.
-            </li>
-            <li>
-              <span className="font-semibold">Authorship</span> The list of authors at the time of submission is final and cannot be changed.
-            </li>
-            <li>
-              <span className="font-semibold">Dual Submissions</span>  DSAA is an archival publication venue; submissions that have been previously published, accepted, or are currently under-review at peer-review publication venues (i.e., journals, conferences, workshops with published proceedings, etc.) are not permitted. DSAA has a strict no dual submission policy.
-            </li>
-            <li>
-              <span className="font-semibold">Conflicts of Interest (COI)</span>  COIs must be declared at the time of submission in the submission system. COIs include employment at the same institution at the time of submission or in the past three years, collaborations during the past three years, advisor/advisee relationships, plus family and close friends. Program chairs are not allowed to submit any papers. Track chairs and special session chairs and other function chairs are not allowed to make submissions to their own tracks.
-            </li>
-            <li>
-              <span className="font-semibold">Attendance</span> At least one author of each accepted paper must register in full and attend the conference to present the paper. No-show papers will be removed from the IEEE Xplore proceedings.
-            </li>
-            <li>
-              <span className="font-semibold">AI-Generated Text</span> The use of artificial intelligence (AI) generated text in an article shall be disclosed in the acknowledgements section of any paper submitted to an IEEE Conference or Periodical. The sections of the paper that use AI-generated text shall have a citation to the AI system used to generate the text.
-            </li>
-          </ol>
-
-          {/* <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-2">
-            For further details, see the <span className="font-semibold">IEEE Proceedings Author Guidelines.</span>
-          </div>
-          <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-2">
-            All submissions will be double-blind reviewed by the Program Committee based on <span className="font-semibold">technical quality, relevance, originality, significance, and clarity.</span> Submissions not complying with formatting or anonymity will be rejected.
-          </div>
-          <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A]">
-            <span className="font-semibold">Note:</span> Non-anonymous technical reports cannot be considered. Exceptions apply for arXiv papers posted at least one month before DSAA&apos;2026 submission deadline, provided the title/abstract differ from the arXiv version.
-          </div> */}
-        </div>
-
+      
         {/* Enquiries */}
         <div className="border border-[#D1D5DB] rounded-xl p-6 md:p-8 mb-6 max-w-5xl mx-auto">
           <h3 className="font-inter font-semibold text-[22px] md:text-[26px] text-[#232B3A] mb-3">
