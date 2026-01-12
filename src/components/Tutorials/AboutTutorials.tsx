9775ca474079e4333033ae92dc3822566f1293fe
Pavan Parvatam <58822492+pavan41299@users.noreply.github.com>
2026-01-12 09:48:14 +0530

    Enhance tutorial guidelines with additional references
    Added links to IEEE Proceedings Author Guidelines for tutorial proposals and survey papers.

--- src/components/Tutorials/AboutTutorials.tsx
+++ src/components/Tutorials/AboutTutorials.tsx
@@ -58,7 +58,15 @@ function AboutTutorials() {
             Tutorial Proposal
           </h3>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-3">
-          Tutorial proposals should be no more than 2 pages and formatted following the standard 2-column U.S. letter style of IEEE Conference template. See the IEEE proceedings author guidelines for further instructions.
+          Tutorial proposals should be no more than 2 pages and formatted following the standard 2-column U.S. letter style of IEEE Conference template. 
+          <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-2">
+            For further details, see the <a
+            href="https://www.ieee.org/conferences/publishing/templates"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#1D4ED8] hover:underline"
+            > IEEE Proceedings Author Guidelines.</a>
+
           </div>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-3">
             The recommended duration of a tutorial is half-day: 2hours:45 minutes.
@@ -82,7 +90,15 @@ function AboutTutorials() {
           Survey Paper
           </h3>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-3">
-          Presenters of the accepted tutorials have the option to submit a survey paper by the deadline specified at the top of this call. Should the presenters choose to submit a survey paper, the list of authors on the survey paper must be identical to the list of presenters in the accepted tutorial. No new authors could be added. The survey paper should be no more than seven (7) pages of technical content plus additional pages solely for references and should be formatted following the standard 2-column U.S. letter style of the IEEE Conference template. See the IEEE Proceedings Author Guidelines. The accepted survey paper will be published as part of the DSAA proceedings. The Tutorial Chairs reserve the rights to reject the survey paper if the quality does not meet the publication standard, and to withdraw the survey paper from the proceedings if the presenter(s) fail to deliver the in-person tutorial in the DSAA conference.
+          Presenters of the accepted tutorials have the option to submit a survey paper by the deadline specified at the top of this call. Should the presenters choose to submit a survey paper, the list of authors on the survey paper must be identical to the list of presenters in the accepted tutorial. No new authors could be added. The survey paper should be no more than seven (7) pages of technical content plus additional pages solely for references and should be formatted following the standard 2-column U.S. letter style of the IEEE Conference template.
+          <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-2">
+            For further details, see the <a
+            href="https://www.ieee.org/conferences/publishing/templates"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#1D4ED8] hover:underline"
+            > IEEE Proceedings Author Guidelines.</a>
+           The accepted survey paper will be published as part of the DSAA proceedings. The Tutorial Chairs reserve the rights to reject the survey paper if the quality does not meet the publication standard, and to withdraw the survey paper from the proceedings if the presenter(s) fail to deliver the in-person tutorial in the DSAA conference.
           </div>
         </div>
 
@@ -109,7 +125,7 @@ function AboutTutorials() {
           Submission Instructions
           </h3>
           <div className="font-inter text-[15px] md:text-[16px] text-[#232B3A] mb-3">
-          Tutorial proposals should be submitted via email to the Tutorials Chairs at <DSAA2026TutorialChairs(at)gmail.com>
+          Tutorial proposals should be submitted via email to the Tutorials Chairs at DSAA2026TutorialChairs(at)gmail.com
           </div>
         </div>
 
